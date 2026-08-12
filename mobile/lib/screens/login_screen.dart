92de279f8c91532476064ab59a1a6bcfd0855659
lefreidk <lefreidk@gmail.com>
2026-08-12 07:16:53 +0100

    Add theme import to login_screen.dart

--- mobile/lib/screens/login_screen.dart
+++ mobile/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../theme.dart';
 import 'package:provider/provider.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:flutter_animate/flutter_animate.dart';
